cac4f0aada22ab0295df157cac2a162e914223a2
TaylorSpencer22 <taylor.spencer2@gmail.com>
2026-06-11 09:50:32 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Mapbox from './components/mapbox'
-import React from 'react'
+
 import './App.css'
 
 
 function App() {
   return (
     <>
-    < Mapbox />
+    < Mapbox /> 
     </>
   )
 }
