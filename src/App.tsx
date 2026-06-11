4122b4371f3d72c447326e21ece399e5c539af1f
TaylorSpencer22 <taylor.spencer2@gmail.com>
2026-06-11 09:43:43 -0500

    mapbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useRef, useEffect, useState } from 'react'
-import mapboxgl from 'mapbox-gl'
-
-import 'mapbox-gl/dist/mapbox-gl.css'
-
+import Mapbox from './components/mapbox'
+import React from 'react'
 import './App.css'
 
+
 function App() {
-
-  const mapRef = useRef<mapboxgl.Map | null>(null)
-  const mapContainerRef = useRef<HTMLDivElement | null>(null)
-  
-  useEffect(() => {
-    mapRef.current = new mapboxgl.Map({
-      accessToken: import.meta.env.VITE_MAPBOX_TOKEN,
-      container: mapContainerRef.current,
-      // style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-99.55446, 36.84276],
-      zoom: 3.22
-    });
-
-    return () => {
-        mapRef.current.remove()
-      }
-  }, []);
-
   return (
     <>
-      <div id='map-container' ref={mapContainerRef} />
+    < Mapbox />
     </>
   )
 }
+
 
 export default App
