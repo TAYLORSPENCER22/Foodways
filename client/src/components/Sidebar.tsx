3bc37f5b59b7f917362cff7b1cc14efd7d81dd96
TaylorSpencer22 <taylor.spencer2@gmail.com>
2026-07-13 22:31:42 -0500

    sidebar styling

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -10,24 +10,32 @@ return (
     <div className="sideBar">
         <div className='cardHeadingContainer'>
         
-             {selectedCountry && ( <button
-                onClick={() => setSelectedCountry(null)} > ← Back </button> )}
-                
-        <h1>{selectedCountry ? selectedCountry.name : 'Explore Countries'}</h1>
-        
-            
+             {selectedCountry && ( <button className='backBtn'
+                onClick={() => setSelectedCountry(null)} > <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#f8ffef" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path></svg>
+                </button> )}
+
+        <div className="countryTitle">
+        <h2>{selectedCountry ? selectedCountry.name : 'Explore Countries'}</h2>
+
+        {selectedCountry && (
+            <img className='individual-country-icon' src={selectedCountry.countryFlagUrl} alt={selectedCountry.name} />
+            )}
+        </div>
 
         <p> {selectedCountry ? `Explore ${selectedCountry.name}'s local favorites` 
             : 'Discover food, culture, and local favorites'}
         </p>
 
+        </div>
+
+        <div className="filterBtnContainer">
         {selectedCountry ? (
             <>
-
                 <button className='filterBtn'>Country</button>
                 <button className='filterBtn'>Regions</button>
                 <button className='filterBtn'>Cities</button>
             </>
+
         ) : (
             <>
                 <button className='filterBtn'>Countries</button>
@@ -35,9 +43,8 @@ return (
                 <button className='filterBtn'>Favorites</button>
             </>
         )}
-
+    </div>
        
-     </div>
         < CountryCardList 
             selectedCountry={selectedCountry}
             setSelectedCountry={setSelectedCountry}
